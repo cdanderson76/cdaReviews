c505964da9681865bb47520520652af40cba1d0b
Clifford <c.andersoninfotech@gmail.com>
2025-02-05 16:18:56 -0600

    made some changes to the dummy data

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export const reviews = [
       job: 'web developer',
       color: 'lightblue',
       image: person1,
-      text: "Hi, I’m Hilda, a front-end developer with a background in graphic design. After spending a decade in the advertising industry, became interested in the technical side of web design and I decided to transition to web development.",
+      text: "Hi, I’m Hilda, a front-end developer with a background in graphic design. After spending a decade in the advertising industry, became interested in the technical side of web design.",
     },
     {
       id: 2,
@@ -18,7 +18,7 @@ export const reviews = [
       job: 'data scientist',
       color: '#dd8245',
       image: person2,
-      text: 'Ze is a Data Scientist at a fintech startup, leveraging her extensive background in accounting to analyze financial trends and build predictive models. After 12 years in the accounting field, she transitioned to data science to pursue her fascination with data analytics.',
+      text: 'Ze is a Data Scientist at a fintech startup, and after 12 years in the accounting field, she transitioned to data science to pursue her fascination with data analytics.',
     },
     {
       id: 3,
@@ -26,7 +26,7 @@ export const reviews = [
       job: 'intern',
       color: '#e6d9ad',
       image: person3,
-      text: 'Hi, I’m Lukas, an aspiring software developer based in Austin, Texas. Although I didn’t attend college, I’ve always been passionate about technology and coding. To pursue my career in tech, I took a proactive approach and used Codecademy to learn programming.',
+      text: 'Hi, I’m Lukas. I didn’t attend college, But I’ve always been passionate about technology and coding. To pursue my career in tech, I took a proactive approach and used Codecademy to learn programming.',
     },
     {
       id: 4,
@@ -34,7 +34,7 @@ export const reviews = [
       job: 'senior dev',
       color: 'blue',
       image: person4,
-      text: 'Hello, I’m Tim Rodd, a machine learning engineer with a background in computer science. I was inspired by the incredible advancements in AI, and using Codecademy, I learned Python and specialized in machine learning with courses on TensorFlow and Scikit-Learn.',
+      text: 'Hello, I’m Tim Rodd, I was inspired by the incredible advancements in AI, and using Codecademy, I learned Python and specialized in machine learning with courses on TensorFlow and Scikit-Learn.',
     },
   ];
   
