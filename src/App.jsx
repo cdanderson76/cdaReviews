d440ad780c3396f7426898f6ab25b464b4ce6c8b
Clifford <c.andersoninfotech@gmail.com>
2025-02-05 13:33:50 -0600

    sending files to repo in Github

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { reviews } from './data';
+import surprise from './images/surpise.png';
+
+export default function App() {
+  return (
+    <div className="canvas">
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+
+  const [ index, setIndex ] = useState(0);
+
+  const currIndex = reviews[index];
+
+  const sepColor = { 
+    backgroundColor: `${currIndex.color}`
+  }
+
+  function addIndex() {
+    if(index < 3) {
+      setIndex(prev => prev + 1);
+    } else if(index === 3) {
+      setIndex(0);
+    }
+  };
+
+  function subIndex() {
+    if(index > 0) {
+      setIndex(prev => prev - 1);
+    } else if(index === 0) {
+      setIndex(3);
+    }
+  };
+
+  function generateRandom() {
+    setIndex(Math.floor(Math.random() * reviews.length));
+  }
+
+  return (
+    <div className="container">
+      <div className='separator' style={sepColor}>
+        <img src={currIndex.image}
+             alt={`profile pic of ${currIndex.name}`} 
+             className='profile-pic'/>
+      </div>
+      <h2>{currIndex.name}</h2>
+      <h3>{currIndex.job}</h3>
+      <div className="text-container">
+        <button className="left-arrow" onClick={subIndex}>⬅</button>
+        <button><img src={surprise} alt="" className="surprise" onClick={generateRandom}/></button>
+        <button className="right-arrow" onClick={addIndex}>➡</button>
+        <p>{currIndex.text}</p>
+        <SliderIcons index={index}/>
+      </div>
+    </div>
+  )
+}
+
+function SliderIcons({index}) {
+
+      return (
+        <div className="slider-container">
+          {reviews.map(item => {
+            const {id} = item;
+            return (
+              <div className={`slider-icon ${index + 1 === id && 'active'}`}></div>
+            )
+          })}
+        </div>
+      )
+}
+
+
---
+++ src/data.js
@@ -0,0 +1,41 @@
+import person1 from './images/person1.jpeg';
+import person2 from './images/person2.jpeg';
+import person3 from './images/person3.jpeg';
+import person4 from './images/person4.jpg';
+
+export const reviews = [
+    {
+      id: 1,
+      name: 'hilda smith',
+      job: 'web developer',
+      color: 'lightblue',
+      image: person1,
+      text: "Hi, I’m Hilda, a front-end developer with a background in graphic design. After spending a decade in the advertising industry, became interested in the technical side of web design and I decided to transition to web development.",
+    },
+    {
+      id: 2,
+      name: 'ze johnson',
+      job: 'data scientist',
+      color: '#dd8245',
+      image: person2,
+      text: 'Ze is a Data Scientist at a fintech startup, leveraging her extensive background in accounting to analyze financial trends and build predictive models. After 12 years in the accounting field, she transitioned to data science to pursue her fascination with data analytics.',
+    },
+    {
+      id: 3,
+      name: 'lukas jones',
+      job: 'intern',
+      color: '#e6d9ad',
+      image: person3,
+      text: 'Hi, I’m Lukas, an aspiring software developer based in Austin, Texas. Although I didn’t attend college, I’ve always been passionate about technology and coding. To pursue my career in tech, I took a proactive approach and used Codecademy to learn programming.',
+    },
+    {
+      id: 4,
+      name: 'tim rodd',
+      job: 'senior dev',
+      color: 'blue',
+      image: person4,
+      text: 'Hello, I’m Tim Rodd, a machine learning engineer with a background in computer science. My journey into machine learning began after completing my degree. I was inspired by the incredible advancements in AI, and using Codecademy, I learned Python and specialized in machine learning with courses on TensorFlow and Scikit-Learn.',
+    },
+  ];
+  
+  export default reviews;
